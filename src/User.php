fd624ed5ce5322bc25d7a8aefe92af7d0d851e27
Lans-D-99 <lans.d.99@protonmail.com>
2024-08-31 08:36:27 +0200

    +User implements IIdentifiableUser

--- src/User.php
+++ src/User.php
@@ -3,20 +3,21 @@ namespace LDLib\User;
 
 use Ds\Set;
 
-abstract class User {
+abstract class User implements IIdentifiableUser {
     public static string $usernameRegex = '/^[\w\-]+$/u';
 
     public readonly int $id;
     public readonly string $username;
     public readonly Set $roles;
-    public readonly \DateTimeImmutable $registrationDate;
 
-    public function __construct(int $id, Set $roles, string $username, \DateTimeImmutable $registrationDate) {
+    public function __construct(int $id, Set $roles, string $username) {
         $this->id = $id;
         $this->roles = $roles;
         $this->username = $username;
-        $this->registrationDate = $registrationDate;
     }
+
+    public function getID():int { return $this->id; }
+    public function hasRole(string $s):bool { return $this->roles->contains($s); }
 }
 
 abstract class UserSettings {
