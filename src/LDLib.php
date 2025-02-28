3f984eb30361900529dda3f9f873ce4ff64c469a
Lans-D-99 <lans.d.99@protonmail.com>
2025-02-28 09:07:44 +0100

    composer: v0.6.0

--- src/LDLib.php
+++ src/LDLib.php
@@ -1,7 +1,7 @@
 <?php
 namespace LDLib;
 
-define('LDLIB_VERSION','v0.5.1');
+define('LDLIB_VERSION','v0.6.0');
 class LDLib {
     public static function init() {
         define('LDLIB_VERSION_WITH_SUFFIX',LDLIB_VERSION.($_SERVER['LDLIB_VERSION_SUFFIX']??''));
