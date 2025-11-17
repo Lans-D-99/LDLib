40c4d9fb979c8cb293cf947fee1cf62d1d0078ce
Lans-D-99 <lans.d.99@protonmail.com>
2025-11-17 19:01:47 +0100

    version 0.16.0

--- src/LDLib.php
+++ src/LDLib.php
@@ -17,7 +17,7 @@
  *****************************************************************************/
 namespace LDLib;
 
-define('LDLIB_VERSION','v0.15.0');
+define('LDLIB_VERSION','v0.16.0');
 class LDLib {
     public static function init() {
         define('LDLIB_VERSION_WITH_SUFFIX',LDLIB_VERSION.($_SERVER['LDLIB_VERSION_SUFFIX']??''));
